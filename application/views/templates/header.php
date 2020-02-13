de5f5fe0f4e59d330397a116e8ad9aedce817c45
Zulfikar Ulya Zen <ulyazen@gmail.com>
2020-02-13 15:09:38 +0700

    adding navbar

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -15,3 +15,5 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 </head>
+
+<body>
---
+++ application/views/templates/navbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-dark bg-primary">
+    <a class="navbar-brand" href="<?= base_url(''); ?>">LOS - BPDDIY</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link" href="<?= base_url('grup'); ?>">Grup <span class="sr-only"></span></a>
+            <a class="nav-item nav-link" href="<?= base_url('kewenangan'); ?>">Kewenangan<span class="sr-only"></span></a>
+            <a class="nav-item nav-link" href="<?= base_url('menu'); ?>">Menu<span class="sr-only"></span></a>
+            <a class="nav-item nav-link" href="<?= base_url('sistem'); ?>">Sistem<span class="sr-only"></span></a>
+            <a class="nav-item nav-link" href="<?= base_url('user'); ?>">User<span class="sr-only"></span></a>
+        </div>
+    </div>
+</nav>
